e3517937b8ecfb6ee0c126df5c638e30385ed8d1
annahaskelsky <83059442+annahaskelsky@users.noreply.github.com>
2021-06-07 16:55:19 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// ReactDOM.render(WHAT TO SHOW , WHERE TO SHOW);
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
+//CAN ONLY TAKE A SINGLE HTML ELEMENT!
